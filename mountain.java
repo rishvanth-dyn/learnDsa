f8c27a92008b821f7789d9cd468aa0c57bdf7dc5
Rishvanth M <rishvanthsvgakvs@gmail.com>
2024-05-19 22:41:03 +0530

    Create mountain.java

---
+++ mountain.java
@@ -0,0 +1,23 @@
+package learing;
+
+public class mountain {
+    public static void main(String args[]) {
+        int[] arr={0,1,0};
+        System.out.println(peakIndexInMountainArray(arr));
+    }
+    public static int peakIndexInMountainArray(int[] arr) {
+        int start = 0;
+        int end = arr.length - 1;
+
+        while (start < end) {
+            int mid = start + (end - start) / 2;
+            if (arr[mid] > arr[mid+1]) {
+                end = mid;
+            } else {
+                start = mid + 1;
+            }
+
+        }
+        return start;
+    }
+}
