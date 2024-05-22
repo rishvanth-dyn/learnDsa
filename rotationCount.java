c2e39fb64599cec73a6b5a19b75d4e57d8dda4db
Rishvanth M <rishvanthsvgakvs@gmail.com>
2024-05-22 20:39:05 +0530

    Create rotationCount.java

---
+++ rotationCount.java
@@ -0,0 +1,32 @@
+package learning;
+
+public class rotationCount {
+    public static void main(String[] args) {
+        int[] arr = {4, 5, 6, 7, 0, 1, 2, 3};
+        System.out.println(countRotation(arr));
+    }
+
+    public static int countRotation(int[] arr) {
+        int answer = findPivot(arr);
+        return answer+1;
+    }
+        public static int findPivot ( int[] arr){
+            int start = 0;
+            int end = arr.length - 1;
+            while (start <= end) {
+                int mid = start + (end - start) / 2;
+                if (mid < end && arr[mid] < arr[mid + 1]) {
+                    return mid;
+                }
+                if (mid > start && arr[mid] > arr[mid - 1]) {
+                    return mid;
+                }
+                if (arr[mid] <= arr[start]) {
+                    end = mid - 1;
+                } else {
+                    start = mid + 1;
+                }
+            }
+            return -1;
+        }
+    }
