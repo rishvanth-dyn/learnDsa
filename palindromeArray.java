76c015698e3476bc1135fdb19324c98ec7d07171
Rishvanth M <rishvanthsvgakvs@gmail.com>
2024-05-29 23:19:19 +0530

    Create palindromeArray.java

---
+++ palindromeArray.java
@@ -0,0 +1,37 @@
+public class palindrome {													@Rishvanth M
+	public static void main(String[] args) {
+		String s[] = new String[]{"madam", "Racecar", "and", "civic", "are", "level", "words" };
+		String ans[] = new String[s.length];
+
+		int index =0;
+
+		for(String str: s){
+			boolean isPalindrome = isPalindrome(str);
+			ans[index] = isPalindrome ? "\n"+str+" ="+" palindrome" : "\n"+str+" ="+" notPalindrome";
+			index++;
+		}
+
+		System.out.println(Arrays.toString(ans));
+	}
+
+	static boolean isPalindrome(String str) {
+        if (str == null || str.length() == 0) {
+            return true;
+        }
+
+        str = str.toLowerCase();
+
+        for (int i = 0; i <= str.length() / 2; i++) 
+		{
+            char start = str.charAt(i);
+            char end = str.charAt(str.length() - 1 - i);
+
+            if (start != end) 
+			{
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
