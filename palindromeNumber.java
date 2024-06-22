4e74d505210f0a56a2874e47a819f46cadedeb4f
Rishvanth M <rishvanthsvgakvs@gmail.com>
2024-06-23 00:05:12 +0530

    Create palindromeNumber.java

---
+++ palindromeNumber.java
@@ -0,0 +1,25 @@
+package learning;
+
+public class palindromeNumber {
+    public static void main(String[] args){
+        int x = 121;
+        System.out.println(isPalindrome(x));
+    }
+
+    public static boolean isPalindrome(int x) {
+        if (x < 0) {
+            return false;
+        }
+
+        long reversed = 0;
+        long temp = x;
+
+        while (temp != 0) {
+            int digit = (int) (temp % 10);
+            reversed = reversed * 10 + digit;
+            temp /= 10;
+        }
+
+        return (reversed == x);
+    }
+}
